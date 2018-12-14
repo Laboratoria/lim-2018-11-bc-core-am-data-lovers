bae3f230dd379ac99c0907d947309f679522800c
MicaelaSuarezCortez <micaelasuarezco@gmail.com>
2018-12-14 11:49:54 -0500

    I added <select> to sort, calculate and <button> for pokemon type. And function to list

--- src/data.js
+++ src/data.js
@@ -1,8 +1,59 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
+/* const example = () => {
   return 'example';
-};
+}; */
+const containerList = document.getElementById('container-list');
+const containerType=document.getElementById('container-list-tipo');
+const btnPoison=document.getElementById('btn-poison');
 
-window.example = example;
+const listData = (data) => {
+  let templateListOfCards = '';
+  // recorremos nuestro array con forEach
+  data.forEach((personaje) => {
+    // creamos un template(string) por cada elemento del array
+    const card = `
+      <div class="card-link">
+        <article class="blog-card">
+          <img class="post-image" src="${personaje.img }" />
+          <div class="article-details">
+            <h4 class="post-category">${personaje.name }</h4>
+            <h3 class="post-title">${ personaje.type }</h3>        
+           
+          </div>
+        </article>
+      </div>
+    `;
+    // vamos concatenando cada li 
+    templateListOfCards += card;
+  })
+  containerList.innerHTML = templateListOfCards;
+}
+
+// ejecuntado funciona para pintar la data en mi html
+listData(POKEMON.pokemon)
+
+//función filtrar
+const filterData=(data,condition)=>{
+let templateListType='';
+
+data.filter((condition =>(condition.type[0]==='Poison'|| condition.type[1]==='Poison')));
+const cardPoison= `<img class="post-image" src="${condition.img }" /> 
+                  <h4 class="post-category">${condition.name }</h4>
+                  <h3 class="post-title">${ condition.type }</h3>`;
+templateListType+=cardPoison;
+}
+containerType.innerHTML=templateListType;
+
+
+
+
+/* filterData(data, condition)
+sortData(data, sortBy, sortOrder)
+computeStats(data) */
+
+window.listData=listData;
+window.filterData=filterData;
+//window.example = example;
+
