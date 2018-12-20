25aaf1eadbb27532fd16b1d7a00cbdefe62a7cad
sofianeyra <safirokpoper@gmail.com>
2018-12-20 13:17:11 -0500

    cambios en html

--- src/data.js
+++ src/data.js
@@ -1,29 +1,10 @@
-//convirtiendo objeto a ARRAY
-var arrayDatos = Object.entries(WORLDBANK);
-
-//clonando ARRAY
-const clonandoDatos = (dato) => {
-    let newArray = [];
-
-for (var i = 0; i < dato.length; i++){
-
-    newArray.push(Object.assign({},dato[i]))
-    //console.log(dato[i])
-
-    } return newArray
-} 
-
-const dataCopia = clonandoDatos(arrayDatos);
+const arrayDatos = Object.entries(WORLDBANK);
 const valoresData = Object.values(dataCopia);
-
-//valores por PAÍS
 
 const PERU = Object.values(valoresData[0]);
 const valuePeru = Object.values(PERU[1]);
-
 const MEXICO = Object.values(valoresData[1]);
 const valueMex = Object.values(MEXICO[1])
-
 const CHILE = Object.values(valoresData[3]);
 const valueChil = Object.values(CHILE[1]);
 
@@ -33,25 +14,68 @@ const data = Object.values(indicadoresPER[0]);
 const dataForYear = Object.values(data[0])
 
 let resultsPERU = dataForYear.filter(number => number > 1);
+const population = WORLDBANK.PER.indicators.filter(function filterData(pop) {
+ return pop.indicatorCode.slice(0,6) === 'SP.POP';
+});
+const violence = WORLDBANK.PER.indicators.filter(pop => {
+ return pop.indicatorCode.slice(0,6) === 'SG.VAW';
+});
+const primaryEducation = WORLDBANK.PER.indicators.filter(pop => {
+ return pop.indicatorCode.slice(0,6) === 'SE.PRM';
+});
+const thirdEducation = WORLDBANK.PER.indicators.filter(pop => {
+ return pop.indicatorCode.slice(0,6) === 'SE.TER';
+});
+const secondEducation = WORLDBANK.PER.indicators.filter(pop => {
+ return pop.indicatorCode.slice(0,6) === 'SE.SEC';
+});
+const inscriptionsEducation = WORLDBANK.PER.indicators.filter(pop => {
+ return pop.indicatorCode.slice(0,6) === 'SE.ENR';
+});
+const unemployment = WORLDBANK.PER.indicators.filter(pop => {
+ return pop.indicatorCode.slice(0,6) === 'SL.UEM'
+});
 
+const populationMex = WORLDBANK.MEX.indicators.filter(pop => {
+ return pop.indicatorCode.slice(0,6) === 'SP.POP'
+});
+const violenceMex = WORLDBANK.MEX.indicators.filter(pop => {
+ return pop.indicatorCode.slice(0,6) === 'SG.VAW'
+});
+const primaryEducationMex = WORLDBANK.MEX.indicators.filter(pop => {
+ return pop.indicatorCode.slice(0,6) === 'SE.PRM'
+});
+const secondEducationMex = WORLDBANK.MEX.indicators.filter(pop => {
+ return pop.indicatorCode.slice(0,6) === 'SE.SEC'
+});
+const thirdEducationMex = WORLDBANK.MEX.indicators.filter(pop => {
+ return pop.indicatorCode.slice(0,6) === 'SE.TER'
+});
+const inscriptionsEducationMex = WORLDBANK.MEX.indicators.filter(pop => {
+ return pop.indicatorCode.slice(0,6) === 'SE.ENR'
+});
+const unemploymentMex = WORLDBANK.MEX.indicators.filter(pop => {
+ return pop.indicatorCode.slice(0,6) === 'SL.UEM'
+});
 
-
-//valuePERU tiene dos elementos,el 0 es un array,1 "strings",
-//dentro de 0, UN OBJETO, con data, countryName, indicatorName
-
-//const indicatorsPeru = Object.keys(PERU);
-//console.log(indicatorsPeru)
-
-
-
-//for (let i = 0; i < PERU.length; i++){
-   //const indicators2 = PERU[i].indicators;
-/*
-   for(let j = 0; j <studentCourses.length; j++)
-    if(studentCourses[j] === number) {
-        console.log(students[i].name);
-    }else {
-
-    } 
-
-*/
+const populationChl = WORLDBANK.CHL.indicators.filter(pop => {
+ return pop.indicatorCode.slice(0,6) === 'SP.POP'
+});
+const violenceChl = WORLDBANK.CHL.indicators.filter(pop => {
+ return pop.indicatorCode.slice(0,6) === 'SG.VAW'
+});
+const primaryEducationChl = WORLDBANK.CHL.indicators.filter(pop => {
+ return pop.indicatorCode.slice(0,6) === 'SE.PRM'
+});
+const secondEducationChl= WORLDBANK.CHL.indicators.filter(pop => {
+ return pop.indicatorCode.slice(0,6) === 'SE.SEC'
+});
+const thirdEducationChl = WORLDBANK.CHL.indicators.filter(pop => {
+ return pop.indicatorCode.slice(0,6) === 'SE.TER'
+});
+const inscriptionsEducationChl = WORLDBANK.CHL.indicators.filter(pop => {
+ return pop.indicatorCode.slice(0,6) === 'SE.ENR'
+});
+const unemploymentChl = WORLDBANK.CHL.indicators.filter(pop => {
+ return pop.indicatorCode.slice(0,6) === 'SL.UEM'
+});
