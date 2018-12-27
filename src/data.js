4b265f5635b7ca0fa66ab8b8e370d93917c5428e
Laura Lozano <lpamelalozano@gmail.com>
2018-12-27 16:03:20 -0500

    applies eslint rules for main.js and data.js

--- src/data.js
+++ src/data.js
@@ -2,27 +2,25 @@
 function filterbyType(data, filterBy, condition) {
   let listIdPokemonType = [];
   switch (filterBy) {
-    case 'Tipo':
-      if (condition == 'all') {
-        listIdPokemonType = data;
-      }
-      else {
-        listIdPokemonType = data.filter(compare => (compare.type[0] === condition || compare.type[1] === condition ||
+  case 'Tipo':
+    if (condition === 'all') {
+      listIdPokemonType = data;
+    } else {
+      listIdPokemonType = data.filter(compare => (compare.type[0] === condition || compare.type[1] === condition ||
           compare.type[2] === condition));
-      }
-      break;
-    case 'Huevo':
-      if (condition == 'all') {
-        listIdPokemonType = data;
-      } else {
-        listIdPokemonType = data.filter(compare => (compare.egg === condition));
-      }
-      break;
-    default: 
+    }
+    break;
+  case 'Huevo':
+    if (condition === 'all') {
+      listIdPokemonType = data;
+    } else {
+      listIdPokemonType = data.filter(compare => (compare.egg === condition));
+    }
+    break;
+  default:
   }
   return listIdPokemonType;
 }
-
 
 
 const filterDataFunction = (data, dataFilter, condition) => {
@@ -37,82 +35,72 @@ const filterDataFunction = (data, dataFilter, condition) => {
     return dataCopy;
 
   switch (condition) {
-    case 0:
-      for (let i = 0; i < dataCopy.length; i++) {
-        arrayFilter.push(dataCopy[i].num);
-        if (arrayFilter[i].indexOf(dataFilter) !== -1)
+  case 0:
+    for (let i = 0; i < dataCopy.length; i++) {
+      arrayFilter.push(dataCopy[i].num);
+      if (arrayFilter[i].indexOf(dataFilter) !== -1)
+        newArrayFilter.push(dataCopy[i]);
+    } break;
+  case 1:
+    for (let i = 0; i < dataCopy.length; i++) {
+      arrayFilter.push(dataCopy[i].name.toLowerCase());
+      if (arrayFilter[i].indexOf(dataFilter.toLowerCase()) !== -1)
+        newArrayFilter.push(dataCopy[i]);
+    }
+    break;
+  case 2:
+    for (let i = 0; i < dataCopy.length; i++) {
+      for (let j = 0; j < dataCopy[i].type.length; j++) {
+        arrayFilter.push(dataCopy[i].type[j].toLowerCase());
+        if (arrayFilter[j].indexOf(dataFilter.toLowerCase()) !== -1)
           newArrayFilter.push(dataCopy[i]);
-      } break;
-    case 1:
-      for (let i = 0; i < dataCopy.length; i++) {
-        arrayFilter.push(dataCopy[i].name.toLowerCase());
-        if (arrayFilter[i].indexOf(dataFilter.toLowerCase()) !== -1)
+      }
+      arrayFilter = [];
+    }
+    break;
+  case 3:
+    for (let i = 0; i < dataCopy.length; i++) {
+      for (let j = 0; j < dataCopy[i].weaknesses.length; j++) {
+        arrayFilter.push(dataCopy[i].weaknesses[j].toLowerCase());
+        if (arrayFilter[j].indexOf(dataFilter.toLowerCase()) !== -1)
           newArrayFilter.push(dataCopy[i]);
       }
-      break;
-    case 2:
-      for (let i = 0; i < dataCopy.length; i++) {
-        for (let j = 0; j < dataCopy[i].type.length; j++) {
-          arrayFilter.push(dataCopy[i].type[j].toLowerCase());
-          if (arrayFilter[j].indexOf(dataFilter.toLowerCase()) !== -1)
-            newArrayFilter.push(dataCopy[i]);
-        }
-        arrayFilter = [];
-      }
-      break;
-    case 3:
-      for (let i = 0; i < dataCopy.length; i++) {
-        for (let j = 0; j < dataCopy[i].weaknesses.length; j++) {
-          arrayFilter.push(dataCopy[i].weaknesses[j].toLowerCase());
-          if (arrayFilter[j].indexOf(dataFilter.toLowerCase()) !== -1)
-            newArrayFilter.push(dataCopy[i]);
-        }
-        arrayFilter = [];
-      }
-      break;
-    default: alert('No existe opcion');
+      arrayFilter = [];
+    }
+    break;
+  default: 
   }
   return newArrayFilter;
 };
 
 
 const sortDataFunction = (data, sortBy, sortOrder) => {
-
   let newArrayFilter = [];
 
   for (let i = 0; i < data.length; i++)
     newArrayFilter.push(Object.assign({}, data[i]));
-
   if (sortBy === 0) {
-    //Ordenar por ID
-    newArrayFilter.sort(
-      function (a, b) {
-        if (sortOrder === 0) return a.id - b.id;
-        else return b.id - a.id;
+    newArrayFilter.sort((idOne, idTwo) => {
+      if (sortOrder === 0) return idOne.id - idTwo.id;
+      else return idTwo.id - idOne.id;
+    });
+  } else {
+    newArrayFilter.sort((namea, nameb) => {
+      if (sortOrder === 0) {
+        if (namea.name > nameb.name) return 1 ;
+        else return -1;
+      } else {
+        if (namea.name < nameb.name) return 1 ;
+        else return -1;
       }
-    );
+    });
   }
-  else {
-    //Ordenar por A-Z
-    newArrayFilter.sort(
-      function (a, b) {
-        if (sortOrder === 0) {
-          if (a.name > b.name) return 1;
-          else return -1;
-        }
-        else {
-          if (a.name < b.name) return 1;
-          else return -1;
-        }
-      }
-    );
-  }
-  return newArrayFilter;
-}
+  return newArrayFilter ;
+};
 
 window.data = {
   filterData: filterDataFunction,
   sortData: sortDataFunction,
   filterbyType,
-}
+};
 
--- src/main.js
+++ src/main.js
@@ -1,71 +1,68 @@
-  const inputSearchText = document.getElementById('input-search-text');
-  const btnSearch = document.getElementById('btn-search');
-  const pokemonCardContainer = document.getElementById('pokemon-container');
-  const filtersByCategories = document.getElementById('pokemon-filters');
-  const sortBy = document.getElementById('pokemon-order');
-  const eggPokemon = document.getElementById('filtrar-huevos');
-  const typePokemon = document.getElementById('pokemon-type');
-  const typeData = window.POKEMON.pokemon;
+const inputSearchText = document.getElementById('input-search-text');
+const btnSearch = document.getElementById('btn-search');
+const pokemonCardContainer = document.getElementById('pokemon-container');
+const filtersByCategories = document.getElementById('pokemon-filters');
+const sortBy = document.getElementById('pokemon-order');
+const eggPokemon = document.getElementById('filtrar-huevos');
+const typePokemon = document.getElementById('pokemon-type');
+const typeData = window.POKEMON.pokemon;
 
-  
+
 const filterInArray = (inputArray) => {
   return inputArray.map(element => {
     return `<label class="badge-${element}">${element}</label>`;
-  }).join(" ");
+  }).join('');
+};
 
-}
-  const functionfilter = (pokemones) => {
-    let templateList = '';
+const functionfilter = (pokemones) => {
+  let templateList = '';
 
-    pokemones.forEach((pokemon) => {
-
-      const card = `
-        <div class="card-link">
-          <article class="blog-card">
-            <img class="post-image" src="${pokemon.img}" />
-            <div class="article-details">
+  pokemones.forEach((pokemon) => {
+    const card = 
+    `
+    <article class="col-md-3 col-sm-4 col-xs-6">
+        <div class="card-link col-md-12 col-sm-12 col-xs-12">
+          <div class="article-details ">
+            <img class="post-image " src="${pokemon.img}" />
               <h3 class="post-title">${pokemon.name}</h3>
-              <p class="post-description">
-                ID: #${pokemon.num}
-                </p>
-                <p class="type-labels">${filterInArray(pokemon.type)}</p>
-              <p class="candy">${pokemon.candy}</p>
-            </div>
-          </article>
+              <p class="post-description">#${pokemon.num} </p>
+              <p class="holo">${filterInArray(pokemon.type)}</p>
+          </div>
         </div>
+      </article>
       `;
-      templateList += card;
-    })
-
-    pokemonCardContainer.innerHTML = templateList;
-  }
-
-  const functionListenFilterOrder = () => {
-    const listenSortBy = sortBy.options[sortBy.selectedIndex].value;
-    const listenFiltersByCategories = filtersByCategories.options[filtersByCategories.selectedIndex].value;
-    const arrayInputFilter = window.data.filterData(window.POKEMON.pokemon, inputSearchText.value, parseInt(listenFiltersByCategories[0]));
-    functionfilter(window.data.sortData(arrayInputFilter, parseInt(listenSortBy[0]), parseInt(listenSortBy[1])));
-    return 1;
-  }
-
-  typePokemon.addEventListener('change', () => {
-    const typeofFilter = 'Tipo';
-    const arrTemp = window.data.filterbyType(typeData, typeofFilter, typePokemon.value);
-    functionfilter(window.data.filterbyType(arrTemp, typeofFilter, typePokemon.value));
+    templateList += card;
   });
 
-  eggPokemon.addEventListener('change', () => {
-    const typeofFilter = 'Huevo';
-    const arrTemp = window.data.filterbyType(typeData, typeofFilter, eggPokemon.value);
-    functionfilter(window.data.filterbyType(arrTemp, 'Tipo', typePokemon.value));
-  });
+  pokemonCardContainer.innerHTML = templateList;
+};
+
+const functionListenFilterOrder = () => {
+  const listenSortBy = sortBy.options[sortBy.selectedIndex].value;
+  const listenFiltersByCategories = filtersByCategories.options[filtersByCategories.selectedIndex].value;
+  const arrayInputFilter = window.data.filterData(window.POKEMON.pokemon, inputSearchText.value, parseInt(listenFiltersByCategories[0]));
+  functionfilter(window.data.sortData(arrayInputFilter, parseInt(listenSortBy[0]), parseInt(listenSortBy[1])));
+  return 1;
+};
+
+typePokemon.addEventListener('change', () => {
+  const typeofFilter = 'Tipo';
+  const arrTemp = window.data.filterbyType(typeData, typeofFilter, typePokemon.value);
+  functionfilter(window.data.filterbyType(arrTemp, typeofFilter, typePokemon.value));
+});
+
+eggPokemon.addEventListener('change', () => {
+  const typeofFilter = 'Huevo';
+  const arrTemp = window.data.filterbyType(typeData, typeofFilter, eggPokemon.value);
+  functionfilter(window.data.filterbyType(arrTemp, 'Tipo', typePokemon.value));
+});
 
 
-  const functionMain = () => {
-    functionListenFilterOrder();
-    filtersByCategories.addEventListener("change", functionListenFilterOrder);
-    sortBy.addEventListener("change", functionListenFilterOrder);
-    btnSearch.addEventListener('click', functionListenFilterOrder);
-  }
+const functionMain = () => {
+  functionListenFilterOrder();
+  filtersByCategories.addEventListener('change', functionListenFilterOrder);
+  sortBy.addEventListener('change', functionListenFilterOrder);
+  btnSearch.addEventListener('click', functionListenFilterOrder);
+};
 
-  functionMain();
+functionMain();
