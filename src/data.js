87d4839fe054c23b54b216d58ea7b6b6edb35654
sofianeyra <safirokpoper@gmail.com>
2019-01-06 01:21:12 -0500

    responsive menu, show indicators with buttons

--- src/data.js
+++ src/data.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 /* global WORLDBANK*/
 // FILTRO DE INDICADORES EN PERÚ POR CATEGORIA: POBLACIÓN, VIOLENCIA, EDUCACIÓN Y EMPLEO.
@@ -6,47 +7,45 @@ const population = WORLDBANK.PER.indicators.filter(pop => {
   return pop.indicatorCode.slice(0, 6) === 'SP.POP';
 });
 
-
-const sortDataPop = population.sort(function (prev, next) {
+const sortDataPop = population.sort(function(prev, next) {
   if (prev.indicatorName > next.indicatorName) {
     return 1;
   }
   if (prev.indicatorName < next.indicatorName) {
-    return -1
+    return -1;
   }
   return 0;
-})
-
+});
 
 const violence = WORLDBANK.PER.indicators.filter(pop => {
   return pop.indicatorCode.slice(0, 6) === 'SG.VAW';
 });
 
-const sortDataVio = violence.sort(function (prev, next) {
+const sortDataVio = violence.sort(function(prev, next) {
   if (prev.indicatorName > next.indicatorName) {
     return 1;
   }
   if (prev.indicatorName < next.indicatorName) {
-    return -1
+    return -1;
   }
   return 0;
-})
+});
 
 const primaryEducation = WORLDBANK.PER.indicators.filter(pop => {
   return pop.indicatorCode.slice(0, 6) === 'SE.PRM';
 });
 
-const sortDataEdu = primaryEducation.sort(function (prev, next) {
+const sortDataEdu = primaryEducation.sort(function(prev, next) {
   if (prev.indicatorName > next.indicatorName) {
     return 1;
   }
   if (prev.indicatorName < next.indicatorName) {
-    return -1
+    return -1;
   }
   return 0;
-})
+});
 
-/*
+
 const thirdEducation = WORLDBANK.PER.indicators.filter(pop => {
   return pop.indicatorCode.slice(0, 6) === 'SE.TER';
 });
@@ -58,35 +57,34 @@ const secondEducation = WORLDBANK.PER.indicators.filter(pop => {
 const inscriptionsEducation = WORLDBANK.PER.indicators.filter(pop => {
   return pop.indicatorCode.slice(0, 6) === 'SE.ENR';
 });
-*/
 
 const unemployment = WORLDBANK.PER.indicators.filter(pop => {
   return pop.indicatorCode.slice(0, 6) === 'SL.UEM';
 });
 
-const sortDataUnem = unemployment.sort(function (prev, next) {
+const sortDataUnem = unemployment.sort(function(prev, next) {
   if (prev.indicatorName > next.indicatorName) {
     return 1;
   }
   if (prev.indicatorName < next.indicatorName) {
-    return -1
+    return -1;
   }
   return 0;
-})
+});
 
 const literacy = WORLDBANK.PER.indicators.filter(pop => {
   return pop.indicatorCode.slice(0, 6) === 'SE.ADT';
 });
 
-const sortDataLit = literacy.sort(function (prev, next) {
+const sortDataLit = literacy.sort(function(prev, next) {
   if (prev.indicatorName > next.indicatorName) {
     return 1;
   }
   if (prev.indicatorName < next.indicatorName) {
-    return -1
+    return -1;
   }
   return 0;
-})
+});
 
 // FILTRO DE INDICADORES POR MEXICO
 
@@ -94,44 +92,44 @@ const populationMex = WORLDBANK.MEX.indicators.filter(pop => {
   return pop.indicatorCode.slice(0, 6) === 'SP.POP';
 });
 
-const sortDataPopMex = populationMex.sort(function (prev, next) {
+const sortDataPopMex = populationMex.sort(function(prev, next) {
   if (prev.indicatorName > next.indicatorName) {
     return 1;
   }
   if (prev.indicatorName < next.indicatorName) {
-    return -1
+    return -1;
   }
   return 0;
-})
+});
 
 const violenceMex = WORLDBANK.MEX.indicators.filter(pop => {
   return pop.indicatorCode.slice(0, 6) === 'SG.VAW';
 });
 
-const sortDataVioMex = violenceMex.sort(function (prev, next) {
+const sortDataVioMex = violenceMex.sort(function(prev, next) {
   if (prev.indicatorName > next.indicatorName) {
     return 1;
   }
   if (prev.indicatorName < next.indicatorName) {
-    return -1
+    return -1;
   }
   return 0;
-})
+});
 
 
 const primaryEducationMex = WORLDBANK.MEX.indicators.filter(pop => {
   return pop.indicatorCode.slice(0, 6) === 'SE.PRM';
 });
 
-const sortDataEduMex = primaryEducationMex.sort(function (prev, next) {
+const sortDataEduMex = primaryEducationMex.sort(function(prev, next) {
   if (prev.indicatorName > next.indicatorName) {
     return 1;
   }
   if (prev.indicatorName < next.indicatorName) {
-    return -1
+    return -1;
   }
   return 0;
-})
+});
 
 /*
 const secondEducationMex = WORLDBANK.MEX.indicators.filter(pop => {
@@ -151,29 +149,29 @@ const unemploymentMex = WORLDBANK.MEX.indicators.filter(pop => {
   return pop.indicatorCode.slice(0, 6) === 'SL.UEM';
 });
 
-const sortDataUnemMex = unemploymentMex.sort(function (prev, next) {
+const sortDataUnemMex = unemploymentMex.sort(function(prev, next) {
   if (prev.indicatorName > next.indicatorName) {
     return 1;
   }
   if (prev.indicatorName < next.indicatorName) {
-    return -1
+    return -1;
   }
   return 0;
-})
+});
 
 const literacyMex = WORLDBANK.PER.indicators.filter(pop => {
   return pop.indicatorCode.slice(0, 6) === 'SE.ADT';
 });
 
-const sortDataLitMex = literacyMex.sort(function (prev, next) {
+const sortDataLitMex = literacyMex.sort(function(prev, next) {
   if (prev.indicatorName > next.indicatorName) {
     return 1;
   }
   if (prev.indicatorName < next.indicatorName) {
-    return -1
+    return -1;
   }
   return 0;
-})
+});
 
 // FILTRO DE INDICADORES EN CHILE
 
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* global WORLDBANK*/
 
 // MENÚ DE NAVEGACIÓN
@@ -13,7 +14,7 @@ const mexb = document.getElementById('mexico-menu');
 const chlb = document.getElementById('chile-menu');
 const brslb = document.getElementById('brasil-menu');
 
-//PANTALLA INICIO
+// PANTALLA INICIO
 inicialb.addEventListener('click', () => {
   inicial.style.display = 'block';
   per.style.display = 'none';
@@ -21,7 +22,7 @@ inicialb.addEventListener('click', () => {
   chl. style.display = 'none';
   brsl.style.display = 'none';
 });
-//PANTALLA PERÚ
+// PANTALLA PERÚ
 perub.addEventListener('click', () => {
   inicial.style.display = 'none';
   per.style.display = 'block';
@@ -29,7 +30,7 @@ perub.addEventListener('click', () => {
   chl. style.display = 'none';
   brsl.style.display = 'none';
 });
-//PANTALLA MÉXICO
+// PANTALLA MÉXICO
 mexb.addEventListener('click', () => {
   inicial.style.display = 'none';
   per.style.display = 'none';
@@ -37,7 +38,7 @@ mexb.addEventListener('click', () => {
   chl. style.display = 'none';
   brsl.style.display = 'none';
 });
-//PANTALLA CHILE
+// PANTALLA CHILE
 chlb.addEventListener('click', () => {
   inicial.style.display = 'none';
   per.style.display = 'none';
@@ -45,7 +46,7 @@ chlb.addEventListener('click', () => {
   chl. style.display = 'block';
   brsl.style.display = 'none';
 });
-//PANTALLA BRASIL
+// PANTALLA BRASIL
 brslb.addEventListener('click', () => {
   inicial.style.display = 'none';
   per.style.display = 'none';
@@ -55,89 +56,89 @@ brslb.addEventListener('click', () => {
 });
 
 
-//-------------------------PERÚ-----------------------------------//
+// ------------------------- PERÚ ----------------------------------- //
 
 // BOTONES PERÚ
 
-const btnPopulation = document.getElementById("boton-population");
-const btnViolence = document.getElementById("boton-violence");
-const btnEducation = document.getElementById("boton-education");
-const btnUnemployment = document.getElementById("boton-unemployment");
-const btnAlfabet = document.getElementById("boton-alfabetizacion");
+const btnPopulation = document.getElementById('boton-population');
+const btnViolence = document.getElementById('boton-violence');
+const btnEducation = document.getElementById('boton-education');
+const btnUnemployment = document.getElementById('boton-unemployment');
+const btnAlfabet = document.getElementById('boton-alfabetizacion');
 
 // divs PERÚ
-const peruPop = document.getElementById("table-peruPop");
-const peruVio = document.getElementById("table-peruVio");
-const peruEdu = document.getElementById("table-peruEdu");
-const peruUnem = document.getElementById("table-peruUnem");
-const peruAlf = document.getElementById("table-peruAlf");
+const peruPop = document.getElementById('table-peruPop');
+const peruVio = document.getElementById('table-peruVio');
+const peruEdu = document.getElementById('table-peruEdu');
+const peruUnem = document.getElementById('table-peruUnem');
+const peruAlf = document.getElementById('table-peruAlf');
 
-//MOSTRAR DATA FILTRADA EN LOS DIVS DEL HTML
+// MOSTRAR DATA FILTRADA EN LOS DIVS DEL HTML
 
-btnPopulation.addEventListener("click", () => {
+btnPopulation.addEventListener('click', () => {
   for (indicador of sortDataPop) {
-     peruPop.innerHTML += indicador.indicatorName;
-     peruVio.style.display = "none";
-     peruEdu.style.display = "none";
-     peruUnem.style.display = "none";
-     peruAlf.style.display = "none";
-     peruPop.style.display = "block";
+    peruPop.innerHTML += indicador.indicatorName;
+    peruVio.style.display = 'none';
+    peruEdu.style.display = 'none';
+    peruUnem.style.display = 'none';
+    peruAlf.style.display = 'none';
+    peruPop.style.display = 'block';
   }
 });
 
-btnViolence.addEventListener("click", () => {
-for (indicador of violence) {
-  peruVio.innerHTML += indicador.indicatorName;
-  peruPop.style.display = "none";
-  peruEdu.style.display = "none";
-  peruUnem.style.display = "none";
-  peruAlf.style.display = "none";
-  peruVio.style.display = "block";
-}
-});
-
-btnEducation.addEventListener("click", () => {
-for (indicador of primaryEducation) {
-  peruEdu.innerHTML += indicador.indicatorName;
-  peruVio.style.display = "none";
-  peruPop.style.display = "none";
-  peruUnem.style.display = "none";
-  peruAlf.style.display = "none";
-  peruEdu.style.display = "block";
+btnViolence.addEventListener('click', () => {
+  for (indicador of violence) {
+    peruVio.innerHTML += indicador.indicatorName;
+    peruPop.style.display = 'none';
+    peruEdu.style.display = 'none';
+    peruUnem.style.display = 'none';
+    peruAlf.style.display = 'none';
+    peruVio.style.display = 'block';
   }
 });
 
-btnUnemployment.addEventListener("click", () => {
+btnEducation.addEventListener('click', () => {
+  for (indicador of primaryEducation) {
+    peruEdu.innerHTML += indicador.indicatorName;
+    peruVio.style.display = 'none';
+    peruPop.style.display = 'none';
+    peruUnem.style.display = 'none';
+    peruAlf.style.display = 'none';
+    peruEdu.style.display = 'block';
+  }
+});
+
+btnUnemployment.addEventListener('click', () => {
   for (indicador of unemployment) {
     peruUnem.innerHTML += indicador.indicatorName;
-    peruVio.style.display = "none";
-    peruEdu.style.display = "none";
-    peruPop.style.display = "none";
-    peruAlf.style.display = "none";
-    peruUnem.style.display = "block";
+    peruVio.style.display = 'none';
+    peruEdu.style.display = 'none';
+    peruPop.style.display = 'none';
+    peruAlf.style.display = 'none';
+    peruUnem.style.display = 'block';
   }
 });
 
-btnAlfabet.addEventListener("click", () => {
+btnAlfabet.addEventListener('click', () => {
   for (indicador of literacy) {
     peruAlf.innerHTML += indicador.indicatorName;
-    peruVio.style.display = "none";
-    peruEdu.style.display = "none";
-    peruUnem.style.display = "none";
-    peruPop.style.display = "none";
-    peruAlf.style.display = "block";
+    peruVio.style.display = 'none';
+    peruEdu.style.display = 'none';
+    peruUnem.style.display = 'none';
+    peruPop.style.display = 'none';
+    peruAlf.style.display = 'block';
   }
 });
 
-//-------------------------MEXICO-----------------------------------//
+// ------------------------- MEXICO ----------------------------------- //
 
 
-//BOTONES MÉXICO
-const btnPopMex = document.getElementById("boton-populationMex");
-const btnVioMex = document.getElementById("boton-violenceMex");
-const btnEduMex = document.getElementById("boton-educationMex");
-const btnUneMex = document.getElementById("boton-unemploymentMex");
-const btnAlfMex = document.getElementById("boton-alfabetizacionMex");
+// BOTONES MÉXICO
+const btnPopMex = document.getElementById('boton-populationMex');
+const btnVioMex = document.getElementById('boton-violenceMex');
+const btnEduMex = document.getElementById('boton-educationMex');
+const btnUneMex = document.getElementById('boton-unemploymentMex');
+const btnAlfMex = document.getElementById('boton-alfabetizacionMex');
 
 // divs MEXICO
 const mexPop = document.getElementById('table-mexPop');
@@ -147,144 +148,144 @@ const mexUnem = document.getElementById('table-mexUnem');
 const mexAlf = document.getElementById('table-mexAlf');
 
 
-//MOSTRAR DATA FILTRADA EN LOS DIVS DEL HTML
+// MOSTRAR DATA FILTRADA EN LOS DIVS DEL HTML
 
-btnPopMex.addEventListener("click", () => {
-for (indicador of populationMex) {
-  mexPop.innerHTML += indicador.indicatorName;
-  mexVio.style.display = "none";
-  mexEdu.style.display = "none";
-  mexUnem.style.display = "none";
-  mexAlf.style.display = "none";
-  mexPop.style.display = "block";
+btnPopMex.addEventListener('click', () => {
+  for (indicador of populationMex) {
+    mexPop.innerHTML += indicador.indicatorName;
+    mexVio.style.display = 'none';
+    mexEdu.style.display = 'none';
+    mexUnem.style.display = 'none';
+    mexAlf.style.display = 'none';
+    mexPop.style.display = 'block';
   }
 });
 
-btnVioMex.addEventListener("click", () => {
-for (indicador of violenceMex) {
-  mexVio.innerHTML += indicador.indicatorName;
-  mexEdu.style.display = "none";
-  mexUnem.style.display = "none";
-  mexAlf.style.display = "none";
-  mexPop.style.display = "none";
-  mexVio.style.display = "block";
-}
+btnVioMex.addEventListener('click', () => {
+  for (indicador of violenceMex) {
+    mexVio.innerHTML += indicador.indicatorName;
+    mexEdu.style.display = 'none';
+    mexUnem.style.display = 'none';
+    mexAlf.style.display = 'none';
+    mexPop.style.display = 'none';
+    mexVio.style.display = 'block';
+  }
 });
 
-btnEduMex.addEventListener("click", () => {
-for (indicador of primaryEducationMex) {
-  mexEdu.innerHTML += indicador.indicatorName;
-  mexUnem.style.display = "none";
-  mexAlf.style.display = "none";
-  mexPop.style.display = "none";
-  mexVio.style.display = "none";
-  mexEdu.style.display = "block";
-}
+btnEduMex.addEventListener('click', () => {
+  for (indicador of primaryEducationMex) {
+    mexEdu.innerHTML += indicador.indicatorName;
+    mexUnem.style.display = 'none';
+    mexAlf.style.display = 'none';
+    mexPop.style.display = 'none';
+    mexVio.style.display = 'none';
+    mexEdu.style.display = 'block';
+  }
 });
 
-btnUneMex.addEventListener("click", () => {
-for (indicador of unemploymentMex) {
-  mexUnem.innerHTML += indicador.indicatorName;
-  mexEdu.style.display = "none";
-  mexVio.style.display = "none";
-  mexAlf.style.display = "none";
-  mexPop.style.display = "none";
-  mexUnem.style.display = "block";
-}
+btnUneMex.addEventListener('click', () => {
+  for (indicador of unemploymentMex) {
+    mexUnem.innerHTML += indicador.indicatorName;
+    mexEdu.style.display = 'none';
+    mexVio.style.display = 'none';
+    mexAlf.style.display = 'none';
+    mexPop.style.display = 'none';
+    mexUnem.style.display = 'block';
+  }
 });
 
-btnAlfMex.addEventListener("click", () => {
-for (indicador of literacyMex) {
-  mexAlf.innerHTML += indicador.indicatorName;
-  mexEdu.style.display = "none";
-  mexUnem.style.display = "none";
-  mexVio.style.display = "none";
-  mexPop.style.display = "none";
-  mexAlf.style.display = "block";
-}
+btnAlfMex.addEventListener('click', () => {
+  for (indicador of literacyMex) {
+    mexAlf.innerHTML += indicador.indicatorName;
+    mexEdu.style.display = 'none';
+    mexUnem.style.display = 'none';
+    mexVio.style.display = 'none';
+    mexPop.style.display = 'none';
+    mexAlf.style.display = 'block';
+  }
 });
 
 
-//-------------------------CHILE-----------------------------------//
+// ------------------------- CHILE ----------------------------------- //
 
-//botones CHILE
+// botones CHILE
 
-const btnPopCh = document.getElementById("boton-populationChil");
-const btnVioCh = document.getElementById("boton-violenceChil");
-const btnEduCh = document.getElementById("boton-educationChil");
-const btnUnemCh = document.getElementById("boton-unemploymentChil");
-const btnAlfCh = document.getElementById("boton-alfabetizacionChil");
+const btnPopCh = document.getElementById('boton-populationChil');
+const btnVioCh = document.getElementById('boton-violenceChil');
+const btnEduCh = document.getElementById('boton-educationChil');
+const btnUnemCh = document.getElementById('boton-unemploymentChil');
+const btnAlfCh = document.getElementById('boton-alfabetizacionChil');
 
 // divs CHILE
-const popCh = document.getElementById("table-popCh");
-const eduCh = document.getElementById("table-eduCh");
-const vioCh = document.getElementById("table-vioCh");
-const unemCh = document.getElementById("table-unemCh");
-const alfCh = document.getElementById("table-alfCh");
+const popCh = document.getElementById('table-popCh');
+const eduCh = document.getElementById('table-eduCh');
+const vioCh = document.getElementById('table-vioCh');
+const unemCh = document.getElementById('table-unemCh');
+const alfCh = document.getElementById('table-alfCh');
 
 
-//MOSTRAR DATA FILTRADA EN LOS DIVS DEL HTML
+// MOSTRAR DATA FILTRADA EN LOS DIVS DEL HTML
 
 
-btnPopCh.addEventListener("click", () => {
+btnPopCh.addEventListener('click', () => {
   for (indicador of populationChl) {
     popCh.innerHTML += indicador.indicatorName;
-    eduCh.style.display = "none";
-    vioCh.style.display = "none";
-    unemCh.style.display = "none";
-    alfCh.style.display = "none";
-    popCh.style.display = "block";
+    eduCh.style.display = 'none';
+    vioCh.style.display = 'none';
+    unemCh.style.display = 'none';
+    alfCh.style.display = 'none';
+    popCh.style.display = 'block';
   }
 });
 
-btnVioCh.addEventListener("click", () => {
+btnVioCh.addEventListener('click', () => {
   for (indicador of violenceChl) {
     vioCh.innerHTML += indicador.indicatorName;
-    eduCh.style.display = "none";
-    popCh.style.display = "none";
-    unemCh.style.display = "none";
-    alfCh.style.display = "none";
-    vioCh.style.display = "block";
+    eduCh.style.display = 'none';
+    popCh.style.display = 'none';
+    unemCh.style.display = 'none';
+    alfCh.style.display = 'none';
+    vioCh.style.display = 'block';
   }
-})
+});
 
-btnEduCh.addEventListener("click", () => {
-  for (indicador of primaryEducationChl){
+btnEduCh.addEventListener('click', () => {
+  for (indicador of primaryEducationChl) {
     eduCh.innerHTML += indicador.indicatorName;
-    popCh.style.display = "none";
-    vioCh.style.display = "none";
-    unemCh.style.display = "none";
-    alfCh.style.display = "none";
-    eduCh.style.display = "block";
+    popCh.style.display = 'none';
+    vioCh.style.display = 'none';
+    unemCh.style.display = 'none';
+    alfCh.style.display = 'none';
+    eduCh.style.display = 'block';
   }
-})
+});
 
-btnUnemCh.addEventListener("click", () => {
+btnUnemCh.addEventListener('click', () => {
   for (indicador of unemploymentChl) {
     unemCh.innerHTML += indicador.indicatorName;
-    eduCh.style.display = "none";
-    vioCh.style.display = "none";
-    popCh.style.display = "none";
-    alfCh.style.display = "none";
-    unemCh.style.display = "block";
+    eduCh.style.display = 'none';
+    vioCh.style.display = 'none';
+    popCh.style.display = 'none';
+    alfCh.style.display = 'none';
+    unemCh.style.display = 'block';
   }
-})
+});
 
-btnAlfCh.addEventListener("click", () => {
+btnAlfCh.addEventListener('click', () => {
   for (indicador of literacyChl) {
     alfCh.innerHTML += indicador.indicatorName;
-    eduCh.style.display = "none";
-    vioCh.style.display = "none";
-    unemCh.style.display = "none";
-    popCh.style.display = "none";
-    alfCh.style.display = "block";
+    eduCh.style.display = 'none';
+    vioCh.style.display = 'none';
+    unemCh.style.display = 'none';
+    popCh.style.display = 'none';
+    alfCh.style.display = 'block';
   }
-})
+});
 
 
-//-------------------------BRASIL-----------------------------------//
+// ------------------------- BRASIL ----------------------------------- //
 
-//botones BRASIL
+// botones BRASIL
 
 const btnPopBra = document.getElementById('boton-populationBra');
 const btnVioBra = document.getElementById('boton-violenceBra');
@@ -294,67 +295,74 @@ const btnAlfBra = document.getElementById('boton-alfabetizacionBra');
 
 // divs CHILE
 
-const braPop = document.getElementById("table-braPop");
-const braEdu = document.getElementById("table-braEdu");
-const braVio = document.getElementById("table-braVio");
-const braUnem = document.getElementById("table-braUnem");
-const braAlf = document.getElementById("table-braAlf");
+const braPop = document.getElementById('table-braPop');
+const braEdu = document.getElementById('table-braEdu');
+const braVio = document.getElementById('table-braVio');
+const braUnem = document.getElementById('table-braUnem');
+const braAlf = document.getElementById('table-braAlf');
 
 
-//MOSTRAR DATA FILTRADA EN LOS DIVS DEL HTML
+// MOSTRAR DATA FILTRADA EN LOS DIVS DEL HTML
 
 
-btnPopBra.addEventListener("click", () => {
+btnPopBra.addEventListener('click', () => {
   for (indicador of populationBrasil) {
     braPop.innerHTML += indicador.indicatorName;
-    braEdu.style.display = "none";
-    braVio.style.display = "none";
-    braUnem.style.display = "none";
-    braAlf.style.display = "none";
-    braPop.style.display = "block";
+    braEdu.style.display = 'none';
+    braVio.style.display = 'none';
+    braUnem.style.display = 'none';
+    braAlf.style.display = 'none';
+    braPop.style.display = 'block';
   }
-})
+});
 
-btnVioBra.addEventListener("click", () => {
+btnVioBra.addEventListener('click', () => {
   for (indicador of violenceBrasil) {
     braVio.innerHTML += indicador.indicatorName;
-    braEdu.style.display = "none";
-    braPop.style.display = "none";
-    braUnem.style.display = "none";
-    braAlf.style.display = "none";
-    braVio.style.display = "block";
+    braEdu.style.display = 'none';
+    braPop.style.display = 'none';
+    braUnem.style.display = 'none';
+    braAlf.style.display = 'none';
+    braVio.style.display = 'block';
   }
-})
+});
 
-btnEduBra.addEventListener("click", () => {
+btnEduBra.addEventListener('click', () => {
   for (indicador of primaryEducationBrasil) {
     braEdu.innerHTML += indicador.indicatorName;
-    braPop.style.display = "none";
-    braVio.style.display = "none";
-    braUnem.style.display = "none";
-    braAlf.style.display = "none";
-    braEdu.style.display = "block";
+    braPop.style.display = 'none';
+    braVio.style.display = 'none';
+    braUnem.style.display = 'none';
+    braAlf.style.display = 'none';
+    braEdu.style.display = 'block';
   }
-})
+});
 
-btnUnemBra.addEventListener("click", () => {
+btnUnemBra.addEventListener('click', () => {
   for (indicador of unemploymentBrasil) {
     braUnem.innerHTML += indicador.indicatorName;
-    braEdu.style.display = "none";
-    braVio.style.display = "none";
-    braPop.style.display = "none";
-    braAlf.style.display = "none";
-    braUnem.style.display = "block";
+    braEdu.style.display = 'none';
+    braVio.style.display = 'none';
+    braPop.style.display = 'none';
+    braAlf.style.display = 'none';
+    braUnem.style.display = 'block';
   }
-})
+});
 
-btnAlfBra.addEventListener("click", () => {
+btnAlfBra.addEventListener('click', () => {
   for (indicador of literacyBrasil) {
     braAlf.innerHTML += indicador.indicatorName;
-    braEdu.style.display = "none";
-    braVio.style.display = "none";
-    braUnem.style.display = "none";
-    braPop.style.display = "none";
-    braAlf.style.display = "block";
+    braEdu.style.display = 'none';
+    braVio.style.display = 'none';
+    braUnem.style.display = 'none';
+    braPop.style.display = 'none';
+    braAlf.style.display = 'block';
   }
-})
+});
+// MENÚ
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function() {
+  mainNav.classList.toggle('active');
+});
